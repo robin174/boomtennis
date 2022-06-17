f2ea4559110fbe86631f8ff7aa416f013b2a1ac7
Robin Green <robin@designingreen.co.uk>
2022-06-17 16:21:16 +0100

    Add GA code

--- header.php
+++ header.php
@@ -30,6 +30,21 @@
 		–––––––––––––––––––––––––––––––––––––––––––––––––– -->
 		<link rel="stylesheet" href="https://use.typekit.net/aam0pps.css">
 
+
+		<!-- To test, from old site
+		–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-136605072-1"></script>
+		<script>
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+
+			gtag('config', 'UA-136605072-1');
+		</script>
+    
+
 		<?php wp_head(); ?>
 	</head>
 
